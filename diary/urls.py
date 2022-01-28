b8c7fe91d8ac1370126e5a0f7c6c3683f8c6b9e4
js10081 <js10081008@gmail.com>
2022-01-28 11:09:59 +0900

    제네릭 뷰 간단하게 바꿈

--- diary/urls.py
+++ diary/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.IndexRedirect.as_view(),name='index'),
     path('diary/',views.PostListView.as_view(),name='page-list'),
-    path('diary/info/',views.info,name='info'),
+    path('diary/info/',views.InfoTemplateView.as_view(),name='info'),
     path('diary/write',views.PageCreateView.as_view(),name='page-create'),
     path('diary/page/<int:pk>/',views.PageDetailView.as_view(),name='page-detail'),
     path('diary/page/<int:pk>/edit/',views.PageUpdateView.as_view(),name='page-update'),
--- diary/views.py
+++ diary/views.py
@@ -1,50 +1,52 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView,ListView,DetailView,UpdateView,DeleteView
+from django.views.generic import (
+    CreateView,ListView,DetailView,UpdateView,DeleteView,TemplateView,RedirectView
+)
 from django.urls import reverse
 from .models import Page
 from .forms import PageForm
 # Create your views here.
 
-def index(request):
-    return render(request,'diary/index.html')
+class IndexRedirect(RedirectView):
+    pattern_name = 'page-list'
 
 class PostListView(ListView):
     model = Page
-    template_name = 'dairy/page_list.html'
+    # ListView에서는 model명_list.html이 default
     ordering = ['-dt_created']
     paginate_by = 8
-    page_kwarg = 'page'
+    # page_kwarg도 'page'가 default
 
 class PageDetailView(DetailView):
     model = Page
-    template_name = 'diary/page_detail.html'
-    pk_url_kwarg = 'page_id'
+    # DetailView에서는 model명_list.html이 default
+    # pk_url_kwarg도 pk가 기본값이기 때문에, urls.py에서 pk로 설정하면 적어주지 않아도 됨.
 
-def info(request):
-    return render(request,'diary/info.html')
+class InfoTemplateView(TemplateView):
+    template_name = 'diary/info.html'
 
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
-    template_name = 'diary/page_form.html'
+    #form_class로 form을 부르면 기본값이 model명_form.html임.
 
     def get_success_url(self) -> str:
-        return reverse('page-detail', kwargs={'page_id':self.object.id})
+        return reverse('page-detail', kwargs={'pk':self.object.id})
 
 class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
-    template_name = 'diary/page_form.html'
-    pk_url_kwarg = 'page_id'
+    #위 CreateView랑 같은 이유
+    # pk_url_kwarg도 pk가 기본값이기 때문에, urls.py에서 pk로 설정하면 적어주지 않아도 됨.
 
     def get_success_url(self) -> str:
-        return reverse('page-detail',kwargs={'page_id':self.object.id})
+        return reverse('page-detail',kwargs={'pk':self.object.id})
 
 
 class PageDeleteView(DeleteView):
     model = Page
-    template_name = 'diary/page_confirm_delete.html'
-    pk_url_kwarg = 'page_id'
+    # DeleteView에서는 model명_confirm_delete.html이 default
+    # pk_url_kwarg도 pk가 기본값이기 때문에, urls.py에서 pk로 설정하면 적어주지 않아도 됨.
 
     def get_success_url(self) -> str:
         return reverse('page-list')
